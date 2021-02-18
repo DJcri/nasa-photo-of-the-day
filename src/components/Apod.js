32e42b86babfaeae95e28ced1688d57a982874c5
DJcri <djcri123@gmail.com>
2021-02-18 10:42:21 -0800

    utilized styled-components and added loading animation

--- src/components/Apod.js
+++ src/components/Apod.js
@@ -1,7 +1,10 @@
 import React, { useState, useEffect } from "react";
+import styled from "styled-components";
 
 import axios from "axios";
 import { BASE_URL, API_KEY } from "../constants";
+
+import Loading from "./Loading";
 
 const Apod = () => {
   const [data, setData] = useState(null);
@@ -19,25 +22,55 @@ const Apod = () => {
   }, []);
 
   if (!data) {
-    return <div>Loading...</div>;
+    return (
+      <ApodContainer>
+        <Loading />
+      </ApodContainer>
+    );
   }
 
   return (
-    <div className="apod">
-      <div className="flex-container">
-        <div className="img-container">
+    <ApodContainer className="apod">
+      <FlexContainer className="flex-container">
+        <ImgContainer className="img-container">
           <h2>Astronomy Picture of the Day</h2>
-          <img src={data.hdurl} alt="astronomy_pic" />
+          <img style={{ width: "100%" }} src={data.hdurl} alt="astronomy_pic" />
           <p className="date">{data.date}</p>
           <p className="author">{`By ${data.copyright}`}</p>
-        </div>
-        <div className="details-container">
+        </ImgContainer>
+        <DetailsContainer className="details-container">
           <h3 className="title">{data.title}</h3>
-          <p className="desc">{data.explanation}</p>
-        </div>
-      </div>
-    </div>
+          <Desc className="desc">{data.explanation}</Desc>
+        </DetailsContainer>
+      </FlexContainer>
+    </ApodContainer>
   );
 };
 
 export default Apod;
+
+const Desc = styled.p`
+  line-height: 32px;
+`;
+
+const ApodContainer = styled.div`
+  background: ${(props) => props.theme.transparentBG};
+  margin: 0 auto;
+  padding: 30px;
+  width: 70%;
+  font-size: 1rem;
+  box-shadow: ${(props) => props.theme.boxShadow};
+  color: white;
+`;
+
+const FlexContainer = styled.div`
+  display: flex;
+  justify-content: space-around;
+`;
+
+const ImgContainer = styled.div`
+  width: 50%;
+`;
+const DetailsContainer = styled.div`
+  width: 40%;
+`;
---
+++ src/theme/index.js
@@ -0,0 +1,6 @@
+const theme = {
+  transparentBG: "rgba(0, 0, 0, 0.2)",
+  boxShadow: "1px 1px 3px 3px rgb(0, 0, 0, 0.5)",
+};
+
+export default theme;
